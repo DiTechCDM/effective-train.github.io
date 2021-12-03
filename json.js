712f99597882c6dfa211674be5f844ad6ab76656
DiTechCDM <85459137+DiTechCDM@users.noreply.github.com>
2021-12-03 15:40:41 +0530

    json.js

--- json.js
+++ json.js
@@ -1,44 +1,7 @@
 
         {
-            "month": "Jan1",
             "revenue": 90,
             "cost": 30,
             "profit": 60
         },
-        {
-            "month": "feb",
-            "revenue": 120,
-            "cost": 50,
-            "profit": 90
-        },
-        {
-            "month": "mar",
-            "revenue": 150,
-            "cost": 80,
-            "profit": 120
-        },
-        {
-            "month": "april",
-            "revenue": 180,
-            "cost": 120,
-            "profit": 150
-        },
-        {
-            "month": "may",
-            "revenue": 200,
-            "cost": 140,
-            "profit": 60
-        },
-        {
-            "month": "June",
-            "revenue": 10,
-            "cost": 30,
-            "profit": 22
-        },
-        {
-            "month": "July",
-            "revenue": 50,
-            "cost": 20,
-            "profit": 60
-        }
   
