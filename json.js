4f18ee893c1d191ee1d4f68f3986e55848df4e03
DiTechCDM <85459137+DiTechCDM@users.noreply.github.com>
2021-12-03 15:51:40 +0530

    json.js

--- json.js
+++ json.js
@@ -1,7 +1,89 @@
 
+{
+    "data": [
         {
+            "id": 1,
+            "month": "January",
             "revenue": 90,
             "cost": 30,
             "profit": 60
         },
-  
+        {
+            "id": 2,
+            "month": "February",
+            "revenue": 90,
+            "cost": 30,
+            "profit": 60
+        },
+        {
+            "id": 3,
+            "month": "March",
+            "revenue": 90,
+            "cost": 30,
+            "profit": 60
+        },
+        {
+            "id": 4,
+            "month": "April",
+            "revenue": 90,
+            "cost": 30,
+            "profit": 60
+        },
+        {
+            "id": 5,
+            "month": "May",
+            "revenue": 90,
+            "cost": 30,
+            "profit": 60
+        },
+        {
+            "id": 6,
+            "month": "June",
+            "revenue": 90,
+            "cost": 30,
+            "profit": 60
+        },
+        {
+            "id": 7,
+            "month": "July",
+            "revenue": 90,
+            "cost": 30,
+            "profit": 60
+        },
+        {
+            "id": 8,
+            "month": "August",
+            "revenue": 90,
+            "cost": 30,
+            "profit": 60
+        },
+        {
+            "id": 9,
+            "month": "September",
+            "revenue": 90,
+            "cost": 30,
+            "profit": 60
+        },
+        {
+            "id": 10,
+            "month": "October",
+            "revenue": 90,
+            "cost": 30,
+            "profit": 60
+        },
+        {
+            "id": 11,
+            "month": "November",
+            "revenue": 90,
+            "cost": 30,
+            "profit": 60
+        },
+        {
+            "id": 12,
+            "month": "December",
+            "revenue": 90,
+            "cost": 30,
+            "profit": 60
+        },
+    ],
+}
