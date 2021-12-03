134f94e4522df4bbece9c4ae33a3e9689b734121
DiTechCDM <85459137+DiTechCDM@users.noreply.github.com>
2021-12-03 14:12:35 +0530

    s

---
+++ json.js
@@ -0,0 +1,19 @@
+{
+    "months_temperature" : [
+        {
+            "date": "Jan",
+            "high": 39,
+            "low": 27
+        },
+        {
+            "date": "feb",
+            "high": 42,
+            "low": 29
+        },
+        {
+            "date": "mar",
+            "high": 46,
+            "low": 32
+        },
+    ]
+}
